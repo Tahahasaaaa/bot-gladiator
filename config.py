e847e2b0dc9bd452d39a75e272fb466247ed451f
taha_hasani <taha.1388.369@gmail.com>
2026-06-14 14:52:12 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # اگر Environment Variable در Railway تعریف شده باشد، از آن استفاده می‌شود
 # در غیر این صورت از مقدار پیش‌فرض زیر استفاده می‌شود
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8955432875:AAEokc4LmDF4R_65wc0iGkgWYJyadEzZEz4")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8955432875:AAHufSyOQ423ChfyiptBSxI4NybeHb1q--A")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "1953490397"))
 
 # محدودیت rate limiting
